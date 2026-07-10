9c42696046ec9f688adce4743c8497d5993960e6
Marck <bjesushc2006@gmail.com>
2026-07-10 16:23:20 -0600

    feat(auth): implement login screen layout with custom curve and form validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
-import 'core/theme/app_text_styles.dart';
+import 'screens/login_screen.dart';
 
 void main() {
   runApp(const NicaLingoApp());
@@ -15,25 +15,7 @@ class NicaLingoApp extends StatelessWidget {
       title: 'NicaLingo',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/icono_app.png',
-                width: 150,
-                height: 150,
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'NicaLingo',
-                style: AppTextStyles.titleLargeNoot,
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: const LoginScreen(),
     );
   }
 }
